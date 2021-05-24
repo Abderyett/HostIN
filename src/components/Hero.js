51dd87c2c7a7423d3309045ce07c282ab7c41f3f
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-05-24 11:20:32 +0100

    add text and button in hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsChevronRight } from 'react-icons/bs';
 import Navbar from './Navbar';
 import dataCenter from '../Svgs/data-center.svg';
 
@@ -11,6 +12,22 @@ function Hero() {
           <div className="hero-text">
             <h1>Cloud Hosting For Pros</h1>
             <h4>Deplay your website with fews clicks</h4>
+            <p>
+              What's more important than fast, reliable, and secure web hosting? It's trust! End up your inconvenience
+              forever by trusting us, and we bet you'll never be dissatisfied!
+            </p>
+            <button type="button" className="btn start-btn">
+              Start Now &nbsp;
+              <span>
+                <BsChevronRight />
+              </span>
+            </button>
+            <button type="button" className="btn contact-btn">
+              Contact Sales &nbsp;
+              <span>
+                <BsChevronRight />
+              </span>
+            </button>
           </div>
           <img className="data-center" src={dataCenter} alt="data-center" />
         </div>
