e9fa9e8c8d90bd7e186696c672effe1363b5dd0d
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-06-11 19:25:34 +0100

    add accordion

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,21 +1,30 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 import React, { useState } from 'react';
 import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 import { faq } from '../faqData';
 
 export function Faq() {
-  const [selected, setSelected] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(null);
+  const toggle = (i) => {
+    if (activeIndex === i) {
+      return setActiveIndex(null);
+    }
+    setActiveIndex(i);
+  };
+
   return (
     <section className="faq-container">
       <h1>Frequently asked questions ?</h1>
       {faq.map((el, index) => (
         <article key={el.id} className="faq-wrapper">
-          <div className="title-wrapper">
-            <h3 className="faq-title">{el.title}</h3>
-            <span>
-              <BiChevronDown className="chevron" />
-            </span>
+          <div className="title-wrapper" onClick={() => toggle(index)}>
+            <p className="faq-title">{el.title}</p>
+            <span className="chevron">{activeIndex === index ? <BiChevronUp /> : <BiChevronDown />}</span>
           </div>
-          <p className="faq-answer">{el.answer}</p>
+          {activeIndex === index && <p className="faq-answer">{el.answer}</p>}
         </article>
       ))}
     </section>
