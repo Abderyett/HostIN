96be3e2494b810c8f5b608fb1c42908861be1ae1
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-06-14 11:25:49 +0100

    style login card

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,15 +5,30 @@ import { Link } from 'react-router-dom';
 export function SignIn() {
   return (
     <div className="form-container">
-      <div className="form-wrapper">
-        <form>
-          <h2>Sign in to your account</h2>
-          <label htmlFor="email"> Email</label>
-          <input id="email" type="email" name="email" />
-          <label htmlFor="password"> Password</label> <Link to="/reset">Forgot your password?</Link>
-          <input id="password" type="password" name="password" />
-          <button type="submit">Contenue</button>
-        </form>
+      <div className="wrap">
+        <div className="form-wrapper">
+          <form>
+            <h2 className="form-heading">Sign in to your account</h2>
+            <div className="email-input">
+              <label htmlFor="email"> Email</label>
+              <input id="email" type="email" name="email" />
+            </div>
+            <div className="password-input">
+              <div className="password-text">
+                <label htmlFor="password"> Password</label>
+
+                <Link className="password-reset" to="/reset">
+                  Forgot your password?
+                </Link>
+              </div>
+
+              <input id="password" type="password" name="password" />
+            </div>
+            <button className="submit-btn" type="submit">
+              Continue
+            </button>
+          </form>
+        </div>
       </div>
       Don't have an account? <Link to="/register">Sign up</Link>
     </div>
