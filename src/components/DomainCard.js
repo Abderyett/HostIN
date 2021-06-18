f6556543092556e024786ae805b101474fccc076
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-06-18 19:51:48 +0100

    add random numberin cards

--- src/components/DomainCard.js
+++ src/components/DomainCard.js
@@ -14,55 +14,60 @@ export function DomainCard() {
               <div className="domain-info">
                 <h2>{searchTerm}</h2>
               </div>
-              <div className="domain-price">
-                <button type="button" className="save-btn">
-                  SAVE 18%
-                </button>
-                <div className="yearly-price">
-                  <div className="old-price">
-                    <span>$10.99</span>
-                  </div>
-                  <div className="current-price">
-                    <span className="dollar-sign">$ </span>1.39 <span>/Month</span>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <button type="button" className="cart-btn">
-                  Add to cart
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <h1 className="option-text">More options</h1>
-        {extensions.map((el, index) => (
-          <div className="card-wrapper">
-            <div className="domain-card-options">
-              <div className="card-content">
-                <div className="domain-info">
-                  <h2>
-                    {searchTerm}
-                    {el}
-                  </h2>
-                </div>
+              <div className="price-wrapper">
                 <div className="domain-price">
-                  <button type="button" className="save-btn">
-                    SAVE {Math.floor(Math.random() * (98 - 61) + 61)}%
+                  <button type="button" className="save-btn-first">
+                    SAVE 18%
                   </button>
                   <div className="yearly-price">
                     <div className="old-price">
                       <span>$10.99</span>
                     </div>
                     <div className="current-price">
-                      <span className="domain-dollar-sign">$ </span>1.39 <span>/Month</span>
+                      <span className="dollar-sign">$ </span>1.39 <span>/Month</span>
                     </div>
                   </div>
                 </div>
                 <div>
-                  <button type="button" className="cart-btn">
+                  <button type="button" className="cart-btn-first">
                     Add to cart
                   </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1 className="option-text">More options</h1>
+        {extensions.map((el, index) => (
+          <div className="card-wrapper" key={index}>
+            <div className="domain-card-options">
+              <div className="card-content">
+                <div className="domain-info">
+                  <h2>
+                    {searchTerm.slice(0, searchTerm.indexOf('.'))}
+                    {el}
+                  </h2>
+                </div>
+                <div className="price-wrapper">
+                  <div className="domain-price">
+                    <button type="button" className="save-btn">
+                      SAVE {Math.floor(Math.random() * (98 - 61) + 61)}%
+                    </button>
+                    <div className="yearly-price">
+                      <div className="old-price">
+                        <span>${Math.round(Math.random() * (67 - 10.99) + 10.99).toFixed(2)}</span>
+                      </div>
+                      <div className="current-price">
+                        <span className="domain-dollar-sign">$ </span>${(Math.random() * (9 - 0.99) + 0.99).toFixed(2)}
+                        <span>/Month</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <button type="button" className="cart-btn">
+                      Add to cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
--- src/tldExtensions.js
+++ src/tldExtensions.js
@@ -1,5 +1,4 @@
 export const extensions = [
-  '.com',
   '.online',
   '.xyz',
   '.link',
